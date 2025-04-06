2eb4d45c83224a2907a9178239f3d4d02502d123
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-04-05 21:33:36 -0300

    sistema de noticias e faq publicas

---
+++ apps/main/home/public.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from django.contrib.auth.decorators import permission_required
+
+from apps.main.faq.models import FAQ
+from apps.main.news.models import News
+
+
+def public_news_list(request):
+    latest_news_list = News.objects.filter(is_published=True, is_private=False).order_by('-pub_date')[:5]
+
+    context = dict()
+    context['latest_news_list'] = latest_news_list
+
+    return render(request, 'pages/public_news_index.html', context)
+
+
+def public_news_detail(request, slug):
+    news = get_object_or_404(News, slug=slug)
+    if news.is_private and not request.user.is_authenticated:
+        raise Http404("Notícia privada não está disponível.")
+    
+    context = dict()
+    context['news'] = news
+
+    return render(request, 'pages/public_news_detail.html', context)
+
+
+def public_faq_list(request):
+    public_faqs = FAQ.objects.filter(is_public=True)
+
+    context = dict()
+    context['public_faqs'] = public_faqs
+
+    return render(request, 'pages/public_faq.html', context)
--- apps/main/home/urls.py
+++ apps/main/home/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 from .views import *
+from .public import *
+
 
 urlpatterns = [
     path('', index, name='index'),
+
     path('profile/edit/', edit_profile, name='edit_profile'),
     path('profile/add-or-edit-address/', add_or_edit_address, name='add_or_edit_address'),
     path('profile/', profile, name='profile'),
+
     path('app/logs/info/', log_info_dashboard, name='log_info_dashboard'),
     path('app/logs/error/', log_error_dashboard, name='log_error_dashboard'),
+
+    path('public/news/', public_news_list, name='public_news_list'),
+    path('public/news/<slug:slug>/', public_news_detail, name='public_news_detail'),
+    path('public/faq/', public_faq_list, name='public_faq_list'),
+
     path('accounts/logout/', logout_view, name="logout"),
 ]
