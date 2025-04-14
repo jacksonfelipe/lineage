24ef69f820c746f5c1edaa24040a8a9334c3aa2c
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-04-14 06:29:33 -0300

    start update locale

--- core/settings.py
+++ core/settings.py
@@ -178,7 +178,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # =========================== INTERNATIONALIZATION CONFIGS ===========================
 
-LANGUAGE_CODE = os.getenv("CONFIG_LANGUAGE_CODE", "pt-br")
+LANGUAGE_CODE = os.getenv("CONFIG_LANGUAGE_CODE", "pt_BR")
 TIME_ZONE = os.getenv("CONFIG_TIME_ZONE", "America/Recife")
 USE_I18N = True
 USE_L10N = True
@@ -191,14 +191,12 @@ TIME_FORMAT = os.getenv("CONFIG_TIME_FORMAT", 'H:i:s')
 GMT_OFFSET = float(os.getenv("CONFIG_GMT_OFFSET", -3))
 
 LANGUAGES = [
-    ('pt-br', _('Português')),
+    ('pt_BR', _('Português')),
     ('en', _('Inglês')),
     ('es', _('Espanhol')),
 ]
 
-LOCALE_PATHS = [
-    BASE_DIR / 'locale',
-]
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 # =========================== STATIC FILES CONFIGS ===========================
 
--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
@@ -30,6 +31,7 @@ urlpatterns = [
     # libs core's
     path('admin/', admin.site.urls),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 # Static/media routes
