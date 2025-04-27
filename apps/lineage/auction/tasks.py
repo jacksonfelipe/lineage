0208b45c7fe8db9c90bb6ea54a61f228f8fc4e23
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-04-26 23:42:54 -0300

    ajustes

--- apps/lineage/auction/tasks.py
+++ apps/lineage/auction/tasks.py
@@ -7,18 +7,16 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def encerrar_leiloes_expirados():
-
     from .services import finish_auction
     from .models import Auction
     from django.utils.timezone import now
 
-    expirados = Auction.objects.filter(end_time__lte=now(), is_active=True)
+    expirados = Auction.objects.filter(end_time__lte=now())
     count = expirados.count()
     for leilao in expirados:
         try:
-            leilao.is_active = False
-            leilao.save()
-            finish_auction(leilao)
+            if leilao.is_active():  # Usando o método is_active()
+                finish_auction(leilao)
         except Exception as e:
             logger.error(f'Erro ao encerrar leilão {leilao.id}: {e}')
     logger.info(f'{count} leilões encerrados automaticamente.')
