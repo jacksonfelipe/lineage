fed761cecc1a2292ce8c59e0f1ec299ac687a8c0
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-05-04 01:23:55 -0300

    ajuste de task

--- apps/lineage/server/tasks.py
+++ apps/lineage/server/tasks.py
@@ -1,10 +1,11 @@
 from celery import shared_task
-from django.utils import timezone
-from .models import PromotionCode, Apoiador
 
 
 @shared_task
 def verificar_cupons_expirados():
+    from apps.lineage.shop.models import PromotionCode
+    from django.utils import timezone
+
     agora = timezone.now()
     cupons_expirados = PromotionCode.objects.filter(ativo=True, validade__lt=agora)
 
