85d7425b0f772b0b241f9f309cb5ba6197b656d6
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-05-13 22:01:22 -0300

    será?

--- apps/lineage/server/signals.py
+++ apps/lineage/server/signals.py
@@ -1,5 +1,7 @@
 from utils.dynamic_import import get_query_class
 LineageAccount = get_query_class("LineageAccount")
 
-
-LineageAccount.ensure_columns()
+try:
+    LineageAccount.ensure_columns()
+except:
+    pass
