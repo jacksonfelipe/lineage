ca3142be3f2118c4cb3ae09d17b1581270861cb5
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-05-28 19:38:06 -0300

    de volta ao inicio kk

--- nginx/ssl_renew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Renova o certificado se estiver próximo de expirar
-docker-compose run --rm certbot renew
-
-# Reinicia o Nginx para carregar o novo certificado
-docker-compose restart nginx 
