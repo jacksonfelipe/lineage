bdd860f6e0f14a0f496f92ad5e560ddef1936c0d
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-05-28 22:50:41 -0300

    ajuste de versao

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ load_dotenv()  # take environment variables from .env.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # System Version
-VERSION = '1.7.2'
+VERSION = '1.7.3'
 
 # Enable/Disable DEBUG Mode
 DEBUG = str2bool(os.environ.get('DEBUG', False))
