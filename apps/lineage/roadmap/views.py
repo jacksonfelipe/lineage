affdd32cd96f7062513bc80ce0412217e06e4885
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-06-20 14:25:31 -0300

    atualização da seleção do idioma e da validação do login para visualizar

--- apps/lineage/roadmap/views.py
+++ apps/lineage/roadmap/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Roadmap, RoadmapTranslation
-from django.utils.translation import get_language
+from .models import Roadmap
+from apps.main.home.decorator import conditional_otp_required
+from django.utils import translation
 
+
+@conditional_otp_required
 def index(request):
     # Exibe apenas itens publicados, não privados, e tradução em português
-    language = 'pt'
+    language = translation.get_language()
     roadmaps = (
         Roadmap.objects.filter(is_published=True, is_private=False)
         .order_by('pub_date')
@@ -12,20 +15,21 @@ def index(request):
     # Para cada roadmap, pega a tradução em português
     items = []
     for roadmap in roadmaps:
-        translation = roadmap.translations.filter(language=language).first()
-        if translation:
+        translation_obj = roadmap.translations.filter(language=language).first() or roadmap.translations.filter(language='pt').first()
+        if translation_obj:
             items.append({
                 'roadmap': roadmap,
-                'translation': translation,
+                'translation': translation_obj,
             })
     return render(request, 'pages/roadmap_index.html', {'items': items, 'segment': 'roadmap'})
 
 
+@conditional_otp_required
 def detail(request, slug):
-    language = 'pt'
+    language = translation.get_language()
     roadmap = get_object_or_404(Roadmap, slug=slug, is_published=True, is_private=False)
-    translation = roadmap.translations.filter(language=language).first()
+    translation_obj = roadmap.translations.filter(language=language).first() or roadmap.translations.filter(language='pt').first()
     return render(request, 'pages/roadmap_detail.html', {
         'roadmap': roadmap,
-        'translation': translation,
+        'translation': translation_obj,
     })
