e873853c352a4c3eeacecb3a24506863c26f0b94
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-06-25 16:15:29 -0300

    teste

--- apps/main/home/tasks.py
+++ apps/main/home/tasks.py
@@ -1,10 +1,12 @@
 from celery import shared_task
-from django.core.mail import send_mail
-from django.conf import settings
 
 
 @shared_task
 def send_email_task(subject, message, from_email, recipient_list):
+
+    from django.core.mail import send_mail
+    from django.conf import settings
+    
     return send_mail(
         subject=subject,
         message=message,
