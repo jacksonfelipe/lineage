7f9cf2cbefb39261699b6626cf1e6e776e9f0d1d
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-08-06 00:06:15 -0300

    update dos dashs

--- apps/lineage/reports/urls.py
+++ apps/lineage/reports/urls.py
@@ -6,6 +6,7 @@ app_name = "reports"
 
 
 urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
     path('inventory/', views.relatorio_movimentacoes_inventario, name='relatorio_movimentacoes_inventario'),
     path('auctions/', views.relatorio_leiloes, name='relatorio_leiloes'),
     path('purchases/', views.relatorio_compras, name='relatorio_compras'),
