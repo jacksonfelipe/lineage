5fe0cd6d918067fecba354cba86bd1f5b77d7753
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2026-02-06 00:10:41 -0300

    Bump version to 1.17.75

--- core/version.py
+++ core/version.py
@@ -1,5 +1,5 @@
-# Versão do sistema — alterada pelo script de release (update_release.bat)
-__version__ = '1.17.50'
+﻿# Versão do sistema — alterada pelo script de release (update_release.bat)
+__version__ = '1.17.75'
 
 
 
